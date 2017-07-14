ce2dfbe82403bcef2cc344169fdef38b9ef3b829
paopao7 <980569038@qq.com>
2017-07-14 17:34:17 +0800

    搭建支付页面

---
+++ AlipayClass/Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+	//该方法为现在支付页面
+    public function index(){
+        $this->display();
+    }
+
+    //
+}
